052dab9430247560566b6a42dbcd325deb9b370c
Glitch (glitch-blank-node) <none>
2021-10-20 17:57:49 +0000

    🍪🌵 Checkpoint ./public/script.js:8362896/5904

--- public/script.js
+++ public/script.js
@@ -17,16 +17,27 @@
 
 window.addEventListener("DOMContentLoaded", evt => {
   const fetchURL = async url => {
-    const response = await axios.get("/fetchURL", { params: { url: url} });
-    return response.json();
+    const response = await axios.get("/fetchURL", { params: { url: url } });
+    return response.data;
   };
 
   const handleSubmit = event => {
     event.preventDefault();
     const url = event.target.elements.url.value;
     fetchURL(url)
-      .then(function(response) {
-        console.log(response);
+      .then(function(data) {
+        console.log(data);
+        // var iframe = document.getElementById("website");
+        // if (iframe && iframe.contentWindow) {
+        //   iframe.contentWindow.postMessage(event.target.value, "*");
+        // }
+        var iframe = document.createElement("iframe");
+        document.body.appendChild(iframe);
+        iframe.contentWindow.document.open();
+        iframe.contentWindow.document.write(data);
+        iframe.contentWindow.document.close();
+        // iframe.src = 'data:text/html;charset=utf-8,' + encodeURI(data);
+        // document.body.appendChild(iframe);
       })
       .catch(error => console.log(error));
   };
