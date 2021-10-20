c169a9534d98bcd93dc0692b57076a6fcf3dfd8f
Glitch (glitch-blank-node) <none>
2021-10-20 16:47:29 +0000

    📉🚨 Checkpoint ./public/script.js:8362896/2078

--- public/script.js
+++ public/script.js
@@ -1,33 +1,43 @@
-function sendToFrame(event) {
-  var iframe = document.getElementById("website")
-  if (iframe && iframe.contentWindow) {
-    iframe.contentWindow.postMessage(event.target.value, "*")
-  }
+// function sendToFrame(event) {
+//   var iframe = document.getElementById("website")
+//   if (iframe && iframe.contentWindow) {
+//     iframe.contentWindow.postMessage(event.target.value, "*")
+//   }
+// }
+
+// window.addEventListener(
+//   "message",
+//   function (event) {
+//     if (event.origin === window.location.origin) {
+//       $("#my-message").text(event.data)
+//     }
+//   },
+//   false
+// )
+
+const  fetchURL =  async (url)=>{
+  try {
+  const response = await fetch(url, {
+    method: 'GET', 
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({"url":url});
+  });
+  return response.json(); 
+}
+} catch (error) {
+  console.log("error fetching")
+}
 }
 
-window.addEventListener(
-  "message",
-  function (event) {
-    if (event.origin === window.location.origin) {
-      $("#my-message").text(event.data)
-    }
-  },
-  false
-)
-
-const fetchURL = async 
-
 const handleSubmit = (event){
-  fetch("/fetchURL")
+  event.preventDefault();
+  const url = event.target.elements.url.value
+  fetchURL(url)
+
 .then(function(response) {
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
-  return response.json();
-})
-.then(function(response) {
-  let objectURL = URL.createObjectURL(response);
-  myImage.src = objectURL;
+  console.log( response)
 });
 }
 
