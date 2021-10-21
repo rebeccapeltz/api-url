447208fd77ff4fd549df9e913d3ce0eca74f4c0e
Glitch (glitch-blank-node) <none>
2021-10-21 00:22:28 +0000

    🐥🖖 Checkpoint ./public/script.js:8362896/10611 ./src/pages/cldapi.hbs:8362896/521

--- public/script.js
+++ public/script.js
@@ -4,58 +4,55 @@ window.addEventListener("DOMContentLoaded", evt => {
     return response.data;
   };
 
-  const updateDOM = (data)=>{
+  const updateDOM = data => {
     // clean up if exists
-        const existing = document.querySelector("#website");
-        if (existing) {
-          document.querySelector("#data").removeChild(existing);
-          document
-            .querySelector("#data")
-            .removeChild(document.querySelector("#h2website"));
-        }
+    const existing = document.querySelector("#website");
+    if (existing) {
+      document.querySelector("#data").removeChild(existing);
+      document
+        .querySelector("#data")
+        .removeChild(document.querySelector("#h2website"));
+    }
 
-        const existingText = document.querySelector("#text");
-        if (existingText) {
-          document.querySelector("#data").removeChild(existingText);
-          document
-            .querySelector("#data")
-            .removeChild(document.querySelector("#h2text"));
-        }
+    const existingText = document.querySelector("#text");
+    if (existingText) {
+      document.querySelector("#data").removeChild(existingText);
+      document
+        .querySelector("#data")
+        .removeChild(document.querySelector("#h2text"));
+    }
 
-        // create new iframe with header
-        const h2website = document.createElement("h2");
-        h2website.setAttribute("id", "h2website");
-        h2website.innerHTML = "Browser";
-        document.querySelector("#data").appendChild(h2website);
+    // create new iframe with header
+    const h2website = document.createElement("h2");
+    h2website.setAttribute("id", "h2website");
+    h2website.innerHTML = "Browser";
+    document.querySelector("#data").appendChild(h2website);
 
-        const iframe = document.createElement("iframe");
-        iframe.setAttribute("width", "800");
-        iframe.setAttribute("height", "400");
-        iframe.setAttribute("id", "website");
-        document.querySelector("#data").appendChild(iframe);
-        iframe.contentWindow.document.open();
-        iframe.contentWindow.document.write(data);
-        iframe.contentWindow.document.close();
+    const iframe = document.createElement("iframe");
+    iframe.setAttribute("width", "800");
+    iframe.setAttribute("height", "400");
+    iframe.setAttribute("id", "website");
+    document.querySelector("#data").appendChild(iframe);
+    iframe.contentWindow.document.open();
+    iframe.contentWindow.document.write(data);
+    iframe.contentWindow.document.close();
 
-        document
-          .querySelector("#data")
-          .appendChild(document.createElement("br"));
+    document.querySelector("#data").appendChild(document.createElement("br"));
 
-        // create new text area with header
-        const h2text = document.createElement("h2");
-        h2text.setAttribute("id", "h2text");
-        h2text.innerHTML = "Text";
-        document.querySelector("#data").appendChild(h2text);
+    // create new text area with header
+    const h2text = document.createElement("h2");
+    h2text.setAttribute("id", "h2text");
+    h2text.innerHTML = "Text";
+    document.querySelector("#data").appendChild(h2text);
 
-        const textArea = document.createElement("textarea");
-        textArea.setAttribute("id", "text");
-        textArea.setAttribute("rows", 20);
-        textArea.setAttribute("cols", 100);
-        textArea.value = data;
-        document.querySelector("#data").appendChild(textArea);
+    const textArea = document.createElement("textarea");
+    textArea.setAttribute("id", "text");
+    textArea.setAttribute("rows", 20);
+    textArea.setAttribute("cols", 100);
+    textArea.value = data;
+    document.querySelector("#data").appendChild(textArea);
+  };
 
-  }
-  
   const handleSubmit1 = event => {
     event.preventDefault();
     const url = event.target.elements.url.value;
@@ -64,65 +61,79 @@ window.addEventListener("DOMContentLoaded", evt => {
         const data =
           typeof resp === "object" ? JSON.stringify(resp, 0, 2) : resp;
         updateDOM(data);
-//         // clean up if exists
-//         const existing = document.querySelector("#website");
-//         if (existing) {
-//           document.querySelector("#data").removeChild(existing);
-//           document
-//             .querySelector("#data")
-//             .removeChild(document.querySelector("#h2website"));
-//         }
+        //         // clean up if exists
+        //         const existing = document.querySelector("#website");
+        //         if (existing) {
+        //           document.querySelector("#data").removeChild(existing);
+        //           document
+        //             .querySelector("#data")
+        //             .removeChild(document.querySelector("#h2website"));
+        //         }
 
-//         const existingText = document.querySelector("#text");
-//         if (existingText) {
-//           document.querySelector("#data").removeChild(existingText);
-//           document
-//             .querySelector("#data")
-//             .removeChild(document.querySelector("#h2text"));
-//         }
+        //         const existingText = document.querySelector("#text");
+        //         if (existingText) {
+        //           document.querySelector("#data").removeChild(existingText);
+        //           document
+        //             .querySelector("#data")
+        //             .removeChild(document.querySelector("#h2text"));
+        //         }
 
-//         // create new iframe with header
-//         const h2website = document.createElement("h2");
-//         h2website.setAttribute("id", "h2website");
-//         h2website.innerHTML = "Browser";
-//         document.querySelector("#data").appendChild(h2website);
+        //         // create new iframe with header
+        //         const h2website = document.createElement("h2");
+        //         h2website.setAttribute("id", "h2website");
+        //         h2website.innerHTML = "Browser";
+        //         document.querySelector("#data").appendChild(h2website);
 
-//         const iframe = document.createElement("iframe");
-//         iframe.setAttribute("width", "800");
-//         iframe.setAttribute("height", "400");
-//         iframe.setAttribute("id", "website");
-//         document.querySelector("#data").appendChild(iframe);
-//         iframe.contentWindow.document.open();
-//         iframe.contentWindow.document.write(data);
-//         iframe.contentWindow.document.close();
+        //         const iframe = document.createElement("iframe");
+        //         iframe.setAttribute("width", "800");
+        //         iframe.setAttribute("height", "400");
+        //         iframe.setAttribute("id", "website");
+        //         document.querySelector("#data").appendChild(iframe);
+        //         iframe.contentWindow.document.open();
+        //         iframe.contentWindow.document.write(data);
+        //         iframe.contentWindow.document.close();
 
-//         document
-//           .querySelector("#data")
-//           .appendChild(document.createElement("br"));
+        //         document
+        //           .querySelector("#data")
+        //           .appendChild(document.createElement("br"));
 
-//         // create new text area with header
-//         const h2text = document.createElement("h2");
-//         h2text.setAttribute("id", "h2text");
-//         h2text.innerHTML = "Text";
-//         document.querySelector("#data").appendChild(h2text);
+        //         // create new text area with header
+        //         const h2text = document.createElement("h2");
+        //         h2text.setAttribute("id", "h2text");
+        //         h2text.innerHTML = "Text";
+        //         document.querySelector("#data").appendChild(h2text);
 
-//         const textArea = document.createElement("textarea");
-//         textArea.setAttribute("id", "text");
-//         textArea.setAttribute("rows", 20);
-//         textArea.setAttribute("cols", 100);
-//         textArea.value = data;
-//         document.querySelector("#data").appendChild(textArea);
+        //         const textArea = document.createElement("textarea");
+        //         textArea.setAttribute("id", "text");
+        //         textArea.setAttribute("rows", 20);
+        //         textArea.setAttribute("cols", 100);
+        //         textArea.value = data;
+        //         document.querySelector("#data").appendChild(textArea);
+      })
+      .catch(error => console.log(error));
+  };
+
+  const handleSubmit2 = event => {
+    event.preventDefault();
+    const url = event.target.elements.url.value;
+    
+    
+    
+    fetchURL(url)
+      .then(function(resp) {
+        const data =
+          typeof resp === "object" ? JSON.stringify(resp, 0, 2) : resp;
+        updateDOM(data);
       })
       .catch(error => console.log(error));
   };
 
   const form1 = document.querySelector("#url-submit");
-  if (form1){
-     form1.addEventListener("submit", handleSubmit1);
+  if (form1) {
+    form1.addEventListener("submit", handleSubmit1);
   }
-   const form2 = document.querySelector("#cld-url");
-  if (form2){
-     form2.addEventListener("submit", handleSubmit2);
+  const form2 = document.querySelector("#cld-url");
+  if (form2) {
+    form2.addEventListener("submit", handleSubmit2);
   }
- 
 });
